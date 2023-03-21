5c1cd255b04d15f2e787a32922a11b7169f4c185
chiaracicco <chiara.cicconetti@unito.it>
2023-03-21 17:04:07 +0100

    update salmon plot - to fix

--- local/src/plot-isoform.r
+++ local/src/plot-isoform.r
@@ -1,74 +1,70 @@
 #! /usr/bin/env Rscript
 
-'RNA-sequencing data visualisation
- 
- plot-isoform.r
- 
- Create barplot and lineplot from counts data 
+# 0. libraries ----
+suppressPackageStartupMessages(library("argparse"))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(reshape2))
+suppressPackageStartupMessages(library(plyr))
 
-Usage:
-   plot-isoform.r [-o <group_order>, -c <condition_col>] <input> <metadata> <group_col> <output>
+# 1. create parser object ----
+parser <- ArgumentParser(description = "Create lineplot from RNA counts data")
 
-Arguments:
-    input  feature count matrix with header
-    metadata  metadata file path
-    group_col  name of metadata column to use for averaging
-    output  output figure prefix
-              
-Options:
-  -c, --condition_col  name of metadata column to use for diverse groups [Default: unique condition for all groups]
-  -o, --group_order_col  name of metadata column to use for ordering groups [Default: alphanumeric order]
-  -p, --palette  custom made palette [Default: RColorBrewer]
+# add options
+parser$add_argument("-c", "--count_matrix", action="store", required = T,
+                    help = "feature count matrix path")
+parser$add_argument("-m", "--metadata", action="store", required = T,
+                    help = "metadata file path")
+parser$add_argument("-f", "--feature", action="store", required = T,
+                    help = "list of feature for count matrix subsetting")
+parser$add_argument("-g", "--group_col", action="store", required = T,
+                    help = "name of metadata column to use for averaging counts")
+parser$add_argument("-d", "--condition_col", action="store",required = T,
+                    help = "name of metadata column to use as line groups")
+parser$add_argument("-t", "--time_point", action="store", required = T,
+                    help = "name of metadata column to use as x axis in lineplot")
+parser$add_argument("--time_order_col", action="store", default = NULL,
+                    help = "name of metadata column to use for x axis ordering [Default: alphanumeric order]")
+parser$add_argument("--pal", action="store", default=NULL,
+                    help = "custom made palette [Default: RColorBrewer]")
 
-Author:
-   Olilab' -> doc
+# create args object
+args <- parser$parse_args()
 
-opts <- docopt(doc)
 
-suppressWarnings(suppressMessages(library(RNAseqRtools)))
-suppressWarnings(suppressMessages(library(docopt)))
+# 2. read input files ----
+counts <- read.delim(args$count_matrix, header = T, row.names = 1)
+features <- scan(arguments$enst_file, character(), quote = "")
+meta <- read.delim(arguments$metadata, header = T)
 
-# File Read ----
-# https://stackoverflow.com/questions/26152998/how-to-make-r-script-takes-input-from-pipe-and-user-given-parameter
-# if the input is stdin one can do 
-# cat fragment.txt | ./plot_atac_frag_distribution.R --poly --pdf stdin  out.pdf
-# cat fragment.txt | ./plot_atac_frag_distribution.R --poly --pdf - out.pdf
-# ./plot_atac_frag_distribution.R --poly --pdf <(cat fragment.txt)  out.pdf
-OpenRead <- function(arg) {
-  if (arg %in% c("-", "/dev/stdin")) {
-    file("stdin", open = "r")
-  } else if (grepl("^/dev/fd/", arg)) {
-    fifo(arg, open = "r")
-  } else {
-    file(arg, open = "r")
-  }
-}
+# filter TPM matrix with enst of interest
+counts <- counts[row.names(counts) %in% features,]
 
-#z <- read.table(OpenRead(arguments$input), header = FALSE)
-z <- read.delim("multi_SalmonQuant.TPM_matrix.DNMT3B.gz", header = T)
-#meta <- read.delim(arguments$metadata, header=T)
-meta <- read.delim("metadata.txt", header = T)
-
-feature_col <- names(z)[1]
 #group <- arguments$group_col
-group_col <- "condition"
+group_col <- "time_condition"
 
-
-toplot <- melt(z, id.vars = feature_col, variable.name = "sample", value.name = "count")
-toplot <- merge(toplot, meta)
-toplot <- ddply(toplot, c(feature_col, group_col), summarize, 
-                av = mean(count), se = sd(count)/sqrt(length(count)), 
+# average across group
+toplot <- reshape2::melt(as.matrix(z), varnames = c("feature","sample"), value.name = "TPM")
+toplot <- merge(toplot, meta, by = "sample")
+toplot <- ddply(toplot, c("feature", group_col)
+                , summarize
+                , av = mean(TPM)
+                , se = sd(TPM)/sqrt(length(TPM)), 
                 .drop = F)
 
-if(!is.null(opt$condition_col)){
-  condition_col <- opt$condition_col
-  toplot <- merge(toplot, meta[, c(group_col,condition_col)])
-  colnames(toplot)[5] <- "cond"
-} else {
-  toplot$cond <- "none"
-}
+# add condition col
+#condition_col <- "time"
+#if(!is.null(opt$condition_col)){
+#  opt$condition_col <- condition_col
+#  toplot <- merge(toplot, meta[, c(group_col, condition_col)])
+#  colnames(toplot)[condition_col] <- "cond"
+#} else {
+#  toplot$cond <- "none"
+#}
+timepoint <- "time"
+condition_col <- "condition"
+toplot <- merge(toplot, meta[,c(group_col,timepoint,condition_col)])
 
-
+# set group order
 if(!is.null(opt$group_order)){
   group_order <- order(meta[,opt$group_order])
   #group_order <- order(meta[,"day_number"])
@@ -77,64 +73,45 @@ if(!is.null(opt$group_order)){
   toplot[,group_col] <- factor(toplot[,group_col])
 }
 
+toplot$time <- factor(toplot$time, levels = c("H9","EBs_Day6","EBs_Day12"))
 
-
-
+# palette ----
+pal <- readRDS("../../../palette.rds")
 
 ##################
 #
 #  Line plot ----
 #
 
-p <- ggplot(toplot, aes_string(x=condition, y="av", color=feature_col, group=feature_col)) +
+p <- ggplot(toplot, aes_string(x=timepoint, y="av", color="feature", group="feature")) +
   geom_line() +
   geom_point() +
-  geom_errorbar(aes(ymax = av + se, ymin = av - se, group=feature_col), width = 0.3, lwd = 0.25) +
+  facet_wrap(.~condition) +
+  #geom_errorbar(aes(ymax = av + se, ymin = av - se, group=feature_col), width = 0.3, lwd = 0.25) +
   theme_pubr(base_size = 8)+
   theme(axis.text.x = element_text(angle = 45,hjust = 1)) +
   xlab("") + ylab("average TPM") +
   scale_color_discrete(name="") +
   scale_y_continuous(trans='log2')
 
-pdf("multi_SalmonQuant.TPM_matrix.DNMT3B_line.pdf", paper="a4", width = 4, height = 4) 
+p <- ggplot(toplot, aes_string(x=timepoint, y="av", color=condition_col, group=condition_col)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(.~feature, scales = "free_y") +
+  #geom_errorbar(aes(ymax = av + se, ymin = av - se, group=feature_col), width = 0.3, lwd = 0.25) +
+  theme_bw(base_size = 8) +
+  theme(axis.text.x = element_text(angle = 45,hjust = 1)) +
+  xlab("") + ylab("average TPM") +
+  scale_color_discrete(type = pal[[condition_col]])
+
+
+pdf("Salmon_plot/DNMT3B.enst.line.pdf", paper="a4", width = 5, height = 4.5) 
 print(p)
 dev.off()
 
-n_group <- length(levels(toplot[,group_col]))
-n_cond <- length(unique(toplot$cond))
 
-# Set color palette
-if(!is.null(opt$pal)){
-  pal <- opt$pal
-} else {
-  available_pals <- c("Greys","Blues","Greens","Oranges","Purples","Reds")
-  pal_all <-list()
-  pals <- list()
-  for(i in 1:n_cond){
-    group_cond <- unique(toplot[toplot$cond==unique(toplot$cond)[i],group_col])
-    pals <- c(RColorBrewer::brewer.pal(length(group_cond), "Blues")
-  }
-  pal[[group_col]] <- pals
-    
-  for (col in available_pals[1:length(cond)]) {
-    pal[[group]][[col]] <- RColorBrewer::brewer.pal()
-  }
-}
 
-pal <- RColorBrewer::brewer.pal(n_group, "Blues")
-names(pal) <- levels(rds$samples[[group]])
 
-pal_default <- list()
-pal_1 <- RColorBrewer::brewer.pal(9, "Blues")[c(3,5,7,9)]
-names(pal_1) <- paste0("shDUBR_",c("NPC","TD_Day10","TD_Day20","TD_Day30"))
-pal_2 <- RColorBrewer::brewer.pal(9, "Greens")[c(3,5,7,9)]
-names(pal_2) <- paste0("shLINC01578_",c("NPC","TD_Day10","TD_Day20","TD_Day30"))
-pal_3 <- RColorBrewer::brewer.pal(9, "Oranges")[c(3,5,7,9)]
-names(pal_3) <- paste0("shMIR99AHG_",c("NPC","TD_Day10","TD_Day20","TD_Day30"))
-pal_4 <- RColorBrewer::brewer.pal(9, "Greys")[c(3,5,7,9)]
-names(pal_4) <- paste0("shPLKO_",c("NPC","TD_Day10","TD_Day20","TD_Day30"))
-
-pal_default[["cond_time"]] <- c(pal_1,pal_2,pal_3,pal_4)
 
                     
 
@@ -164,8 +141,6 @@ dev.off()
 
 
 
-
-# Stacked barplot
 
 
 
