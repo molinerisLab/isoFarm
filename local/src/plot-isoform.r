a78abbca52abd60115b9f124c62f418c6beb7003
chiaracicco <chiara.cicconetti@unito.it>
2023-04-03 15:12:40 +0200

    major update plot-isoform

--- local/src/plot-isoform.r
+++ local/src/plot-isoform.r
@@ -1,12 +1,15 @@
 #! /usr/bin/env Rscript
 
+########
 # 0. libraries ----
 suppressPackageStartupMessages(library(argparse))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(reshape2))
 suppressPackageStartupMessages(library(plyr))
 
+############
 # 1. create parser object ----
+
 parser <- ArgumentParser(description = "Create lineplot from RNA counts data")
 # add options
 parser$add_argument("-c", "--count_matrix", action="store", required = T,
@@ -21,122 +24,146 @@ parser$add_argument("-d", "--condition_col", action="store",required = T,
                     help = "name of metadata column to use as line groups")
 parser$add_argument("-t", "--time_point", action="store", required = T,
                     help = "name of metadata column to use as x axis in lineplot")
-parser$add_argument("--time_order_col", action="store", default = NULL,
+parser$add_argument("--group_order", action="store", default = NULL,
                     help = "name of metadata column to use for x axis ordering [Default: alphanumeric order]")
 parser$add_argument("--pal", action="store", default=NULL,
                     help = "custom made palette as RDS [Default: RColorBrewer]")
 parser$add_argument("--outdir", action="store", default=".",
                     help = "output directory [Default: .]")
+parser$add_argument("--gene_id_file", action="store_true", default=NULL,
+                    help = "file path with 2 columns: enst;geneID [Default: NULL]")
 # create args object
 args <- parser$parse_args()
 
 
+
+########
 # 2. assign arguments to obj  ----
+
+# create outdir
 if(!is.null(opt$outdir)) outdir <- opt$outdir else outdir <- "."
 if(!dir.exists(outdir)) dir.create(outdir, recursive = T)
-#counts <- read.delim(args$count_matrix, header = T, row.names = 1)
-#features <- scan(arguments$enst_file, character(), quote = "")
-#meta <- read.delim(arguments$metadata, header = T)
-#group <- arguments$group_col
+# read input files
+counts <- read.delim(opt$count_matrix, header = T, row.names = 1)
+features <- scan(opt$feature, character(), quote = "")
+meta <- read.delim(opt$metadata, header = T)
 
-# files
-counts <- read.delim("multi_SalmonQuant.TPM_matrix.gz", header = T, row.names = 1)
-features <- scan("Salmon_plot/SOX5.enst", character(), quote = "")
-meta <- read.delim("metadata.txt", header = T)
 
-# other args
-group_col <- opt$group_col
-condition_col <- opt$condition_col
+
+##########
+# 3. create toplot obj ----
 
 # convert all "-" to "."
 meta <- as.data.frame(apply(meta, 2, function(x) gsub(x, pattern = "-", replacement = ".")))
-
 # filter TPM matrix with enst of interest
 counts <- counts[row.names(counts) %in% features,]
-
 # average across group
 toplot <- reshape2::melt(as.matrix(counts), varnames = c("feature","sample"), value.name = "value")
 toplot <- merge(toplot, meta, by = "sample")
+colnames(toplot)[which(colnames(toplot)==opt$group_col)] <- "group_col"
 
-toplot$group <- toplot[, opt$group_col]
-toplot <- ddply(toplot, .(feature, group), mutate, av = mean(value), sd = sd(value))
-
-
-#toplot <- ddply(toplot, .(feature, group), summarize, av = mean(TPM), se = sd(TPM)/sqrt(length(TPM)), .drop = F)
-
-
-
-toplot$group <- NULL
-if (!is.null(ord_idx)) 
-  toplot[, group.by] <- factor(toplot[, group.by], levels = ord_idx)
-
-
-
-
-
-
-
-
-
-
-
-
-
-meta_cols <- c("sample",group_col)
-if(!is.null(opt$timepoint)){
-  meta_cols <- c(meta_cols,opt$timepoint)
-}
-if(!is.null(opt$condition_col)){
-  meta_cols <- c(meta_cols,opt$condition_col)
+if(!is.null(opt$gene_id_file)){
+  r <- read.delim(opt$gene_id_file, header = F, sep = ";")
+  toplot <- merge(toplot, r, by.x="feature", by.y="V1")
+  colnames(toplot)[which(colnames(toplot)=="feature")] <- "feature_enst"
+  colnames(toplot)[which(colnames(toplot)=="V2")] <- "feature"
 }
 
-toplot <- merge(toplot, meta[,meta_cols])
+toplot <- ddply(toplot, .(feature, group_col), mutate, av = mean(value), sd = sd(value))
 
+#toplot$group_col <- NULL
+#if (!is.null(opt$time_order_col)){
+#  toplot[, group.by] <- factor(toplot[, group.by], levels = ord_idx)
+#}
+
+#meta_cols <- c("sample",group_col)
+#if(!is.null(opt$timepoint)){
+#  meta_cols <- c(meta_cols,opt$timepoint)
+#}
+#if(!is.null(opt$condition_col)){
+#  meta_cols <- c(meta_cols,opt$condition_col)
+#}
+#toplot <- merge(toplot, meta[,meta_cols])
+
+# set x axis and faceting columns
 if(!is.null(opt$timepoint)){
   colnames(toplot)[which(colnames(toplot)==opt$timepoint)] <- "x_axis"
 } else {
   colnames(toplot)[which(colnames(toplot)==opt$group_col)] <- "x_axis"
 }
 if(!is.null(opt$condition_col)){
-  colnames(toplot)[which(colnames(toplot)==opt$condition_col)] <- "condition"
+  colnames(toplot)[which(colnames(toplot)==opt$condition_col)] <- "condition_faceting"
 }
 
-# set group order ----
+# set group order
 if(!is.null(opt$group_order)){
   group_order <- order(meta[,opt$group_order])
-  toplot$x_axis <- factor(toplot$x_axis, levels = unique(meta$x_axis[group_order]))
+  if(!is.null(opt$timepoint)){
+    toplot$x_axis <- factor(toplot$x_axis, levels = unique(meta[group_order,opt$timepoint]))
+  } else {
+    toplot$x_axis <- factor(toplot$x_axis, levels = unique(meta[group_order,opt$group_col]))
+  }
 } else {
   toplot[,group_col] <- factor(toplot[,group_col])
 }
 
+
+
+#########
 # palette ----
+
 if(!is.null(opt$pal)){
   pal <- readRDS(opt$pal)
 } else {
   library(RColorBrewer)
-  pal <- brewer.pal(n = length(levels(toplot$x_axis)), name = "Set1")
-  names(pal) <- levels(toplot$x_axis)
+  pal <- list()
+  pal_cond <- brewer.pal(n = length(levels(toplot$condition_faceting)), name = "Set1")
+  names(pal_cond) <- levels(toplot$condition_faceting)
+  pal_feature <- c(brewer.pal(n=8, name = "Dark2"), brewer.pal(n=8, name = "Set1"))
+  names(pal_feature) <- unique(toplot$feature)
+  pal[["condition_faceting"]] <- pal_cond
+  pal[["feature"]] <- pal_feature
 }
 
 
 
 # 2. Line plot ----
-p_line <- ggplot(toplot, aes(x=x_axis, y=av, color=feature, group=feature)) +
-  geom_line() +
-  geom_point() +
-  geom_errorbar(aes(ymax = av + se, ymin = av - se, group=feature_col), width = 0.3, lwd = 0.25) +
-  theme_pubr(base_size = 8) +
+p_line_cond_facet <- ggplot(toplot, aes(x=x_axis, y=av, color=feature, group=feature)) +
+  geom_line(linewidth=0.3) +
+  geom_point(size=0.7) +
+  geom_errorbar(aes(ymax = av + sd, ymin = av - sd, group=feature), width = 0.2, lwd = 0.25, size = 0.2) +
+  theme_classic(base_size = 8) +
   theme(axis.text.x = element_text(angle = 45,hjust = 1)) +
   xlab("") + ylab("average TPM") +
-  scale_color_discrete(name="") 
+  facet_wrap(.~condition_faceting, nrow = length(unique(toplot$condition_faceting))) + 
+  scale_color_manual(name="", values = pal[["feature"]])
 
 if(!is.null(opt$condition_col)){
-  p_line <- p_line + facet_wrap(.~condition)
+  p_line_cond_facet_scaled <- p_line_cond_facet + 
+    facet_wrap(.~condition_faceting, scales = "free_y", nrow = length(unique(toplot$condition_faceting)))
+  p_line_cond_facet <- p_line_cond_facet +
+    facet_wrap(.~condition_faceting, nrow = length(unique(toplot$condition_faceting)))
 }
+
+p_line_feature_facet <- ggplot(toplot, aes(x=x_axis, y=av, color=condition_faceting, group=condition_faceting)) +
+  geom_line(linewidth=0.3) +
+  geom_point(size=0.7) +
+  facet_wrap(.~feature, scales = "free_y") +
+  geom_errorbar(aes(ymax = av + sd, ymin = av - sd, group=feature), width = 0.3, lwd = 0.25, size = 0.2) +
+  theme_classic(base_size = 8) +
+  theme(axis.text.x = element_text(angle = 45,hjust = 1)) +
+  xlab("") + ylab("average TPM") +
+  scale_color_manual(values = pal[["condition_faceting"]], name="") 
+
 # save pdf
-pdf(paste0(outdir,"/line_plot.pdf"), paper="a4", width = 5, height = 4.5) 
-print(p)
+pdf(paste0(outdir,"/line_plot.pdf"), paper="a4", width = 4, height = 4.5) 
+print(p_line_cond_facet)
 dev.off()
+
+pdf(paste0(outdir,"/line_plot_feature.pdf"), paper="a4", width = 5, height = 4.5) 
+print(p_line_feature_facet)
+dev.off()
+
 
 
 # 3. bar plot with transcripts faceting ----
